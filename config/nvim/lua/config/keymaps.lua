ed903f84e9970d55aac28d08c57c6fb31b5e0f9c
baepaul <paul@gitdot.io>
2025-12-30 13:52:43 -0500

    cleaning up dotfiles significantly...

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true           -- Line numbers
+vim.opt.relativenumber = true   -- Relative line numbers
+vim.opt.mouse = 'a'             -- Enable mouse
+vim.opt.ignorecase = true       -- Case insensitive search
+vim.opt.smartcase = true        -- Unless uppercase is used
+vim.opt.expandtab = true        -- Use spaces instead of tabs
+vim.opt.shiftwidth = 2          -- Indent width
+vim.opt.tabstop = 2             -- Tab width
+vim.opt.termguicolors = true    -- True color support
+
+vim.keymap.set({'n', 'v'}, ';', ':', { desc = 'Command mode' })
+vim.keymap.set({'n', 'v'}, ':', ';', { desc = 'Repeat f/t' })
