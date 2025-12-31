5f527d4ff7894b14a9d6e45f5cab0da6cfe6e663
baepaul <paul@gitdot.io>
2025-12-31 15:04:31 -0500

    just fzf

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,5 +8,15 @@ vim.opt.shiftwidth = 2          -- Indent width
 vim.opt.tabstop = 2             -- Tab width
 vim.opt.termguicolors = true    -- True color support
 
-vim.keymap.set({'n', 'v'}, ';', ':', { desc = 'Command mode' })
-vim.keymap.set({'n', 'v'}, ':', ';', { desc = 'Repeat f/t' })
+local keymap = vim.keymap.set
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+keymap({'n', 'v'}, ';', ':', { desc = 'Command mode' })
+keymap({'n', 'v'}, ':', ';', { desc = 'Repeat f/t' })
+keymap({'n', 'v'}, '<leader>f', '<cmd>lua require("fzf-lua").files()<CR>', { desc = 'Find files' })
+keymap({'n', 'v'}, '<leader>g', '<cmd>lua require("fzf-lua").live_grep()<CR>', { desc = 'Ripgrep' })
+keymap({'n', 'v'}, '<leader>e', '<cmd>lua require("fzf-lua").buffers()<CR>', { desc = 'Recent buffers' })
+keymap({'n', 'v'}, '<leader>p', '<cmd>lua require("fzf-lua").commands()<CR>', { desc = 'Commands' })
+
